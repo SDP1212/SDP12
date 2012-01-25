4886cc59faff5b613d87f86036d478d465f3f67f
Diana Crisan <s0907806@corran.inf.ed.ac.uk>
2012-01-25 09:13:08 +0000

    Trying things out

---
+++ src/computer/PenaltyMode.java
@@ -0,0 +1,13 @@
+package computer;
+
+import lejos.nxt.*;
+import lejos.robotics.*;
+
+public class PenaltyMode {
+    
+    public static void main (String [] args){
+         Motor.C.rotate(-30);
+    }
+    
+    
+}
