d8dce9ba23a03c8899a431e336c40e1519f38896
Matt Jeffryes <m.j.jeffryes@sms.ed.ac.uk>
2012-01-26 17:11:20 +0000

    Script to run application

---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+java -classpath "/group/teaching/sdp/sdp12/secure/lejos_nxj/lib/pc/3rdparty/bluecove-gpl.jar:/group/teaching/sdp/sdp12/secure/lejos_nxj/lib/pc/3rdparty/bluecove.jar:/group/teaching/sdp/sdp12/secure/lejos_nxj/lib/pc/pccomm.jar:build" computer.ApplicationController
