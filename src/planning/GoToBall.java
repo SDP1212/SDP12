4df0c0dd7e13926d525dca61cedb1ec40573d653
Diana Crisan <crisan_diana@ymail.com>
2012-02-08 14:19:52 +0000

    Added initial folder structure for planning

---
+++ src/planning/GoToBall.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package planning;
+
+import vision.*;
+/**
+ *
+ * @author s0907806
+ */
+public class GoToBall {
+    
+    public void findBallLocation () {
+
+    }
+    
+    
+}
