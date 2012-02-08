2b9141c50a167fa92902072dadb2f6816e203f2f
Diana Crisan <crisan_diana@ymail.com>
2012-02-08 14:47:34 +0000

    Trying out some things with planning

--- src/planning/GoToBall.java
+++ src/planning/GoToBall.java
@@ -4,15 +4,48 @@
  */
 package planning;
 
-import vision.*;
+import computer.simulator.PixelCoordinates;
+import vision.WorldState;
 /**
  *
  * @author s0907806
  */
+
+/**
+ * This class will get the ball coordinates and direct the robot towards it.
+ */
+
 public class GoToBall {
     
-    public void findBallLocation () {
-
+    private static WorldState worldState;
+    
+    public PixelCoordinates blue;
+    public PixelCoordinates yellow;
+    public PixelCoordinates ball;
+    
+    public GoToBall (boolean isBlue) {
+        worldState = new WorldState();
+        if (isBlue == true) {
+            this.blue = findBlueRobot();
+        } else {
+            this.yellow = findYellowRobot();
+        }
+        this.ball = findBallLocation();
+    }
+    
+    public PixelCoordinates findBallLocation () {
+        PixelCoordinates loc = worldState.getBallCoordinates();
+        return loc;
+    }
+    
+    public PixelCoordinates findBlueRobot () {
+        PixelCoordinates loc = worldState.getBlueRobotCoordinates();
+        return loc;
+    }
+    
+    public PixelCoordinates findYellowRobot () {
+        PixelCoordinates loc = worldState.getYellowRobotCoordinates();
+        return loc;
     }
     
     
