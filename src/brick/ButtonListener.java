e70a404fba22a19d12ebae7d421499aa19d91e6f
Matt Jeffryes <m.j.jeffryes@sms.ed.ac.uk>
2012-02-09 17:18:19 +0000

    So sorry

---
+++ src/brick/ButtonListener.java
@@ -0,0 +1,20 @@
+package brick;
+
+import lejos.nxt.Button;
+import lejos.nxt.comm.Bluetooth;
+
+/**
+ *
+ * @author Matthew Jeffryes
+ */
+public class ButtonListener implements lejos.nxt.ButtonListener  {
+
+    public void buttonPressed(Button button) {
+        
+    }
+
+    public void buttonReleased(Button button) {
+        Bluetooth.cancelConnect();
+    }
+    
+}
