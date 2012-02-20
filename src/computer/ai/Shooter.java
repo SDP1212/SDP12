a33b7dfb197d9fc49b3b3e35b1ee394b2fe383be
Matt Jeffryes <m.j.jeffryes@sms.ed.ac.uk>
2012-02-20 17:53:21 +0000

    State machine for Shooter AI

--- src/computer/ai/Shooter.java
+++ src/computer/ai/Shooter.java
@@ -15,7 +15,12 @@ import java.util.Date;
  * @author Matt Jeffryes
  */
 public class Shooter extends AI {
+	private static final int SEARCHING = 0;
+	private static final int DRIBBLING = 1;
+	private static final int SHOT = 2;
+	
 	private Date shotTime = new Date(0);
+	private int state = SEARCHING;
 
 	public Shooter(Pitch pitch, Robot self) {
 		super(pitch, self);
@@ -26,18 +31,61 @@ public class Shooter extends AI {
 	
 	@Override
 	public void run() {
-		Goal goal = pitch.getTargetGoal();
-		Box shootingBox = new Box(goal.getLowerPostCoordinates(), 
-				new Coordinates(
-				goal.getUpperPostCoordinates().getX() + 0.5, goal.getUpperPostCoordinates().getY()));
-		if (shootingBox.isPointInside(self.getPosition()) && (new Date().getTime() - shotTime.getTime() > 2000)) {
-			self.kick();
-			shotTime = new Date();
+//		Goal goal = pitch.getTargetGoal();
+//		Box shootingBox = new Box(goal.getLowerPostCoordinates(), 
+//				new Coordinates(
+//				goal.getUpperPostCoordinates().getX() + 0.5, goal.getUpperPostCoordinates().getY()));
+//		if (shootingBox.isPointInside(self.getPosition()) && (new Date().getTime() - shotTime.getTime() > 2000)) {
+//			self.kick();
+//			shotTime = new Date();
+//		}
+		updateState();
+	}
+	
+	private void updateState() {
+		switch (state) {
+			case SEARCHING :
+				if (facingBall() && nearBall()) {
+					state = DRIBBLING;
+				}
+				break;
+			case DRIBBLING :
+				if (!facingBall() || !nearBall()) {
+					state = SEARCHING;
+				} else if (new Date().getTime() - shotTime.getTime() < 1500) {
+					state = SHOT;
+				}
+				break;
+			case SHOT :
+				if (new Date().getTime() - shotTime.getTime() >= 1500) {
+					state = SEARCHING;
+				}
+				break;
+		}
+		System.out.println("State: " + state);
+	}
+	
+	private boolean facingBall() {
+		Line lineToBall = new Line(self.getPosition(), pitch.ball.getPosition());
+		double angle = LineTools.angleBetweenLineAndDirection(lineToBall, self.getOrientation());
+		if (Math.abs(angle) < Math.PI / 16) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private boolean nearBall() {
+		Line lineToBall = new Line(self.getPosition(), pitch.ball.getPosition());
+		if (lineToBall.getLength() < 0.2) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
 	@Override
-	public void robotCollided() {
+	public synchronized void robotCollided() {
 		
 	}
 	
